f1ce453ece178202bac55a935bb95f34878f9a84
DrMacinyasha <drmacinyasha@gmail.com>
2011-04-05 12:29:38 -0700

    Update manifest.js
    JSON compliance

--- manifest.js
+++ manifest.js
@@ -1,258 +1,252 @@
 {
-    version: 1,
-    homepage: "http://goo-inside.me/supersonic/",
-    donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KNWA22ZGW32VS",
-    roms:
+    "version": "1",
+    "homepage": "http://www.goo-inside.me/supersonic/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=KNWA22ZGW32VS",
+    "roms":
     [
-    // ROMs
     {
-        name: "Stock 3.70.651.1 Deodexed",
-        summary: "Rooted, zipaligned, with busybox, from teshxx",
-        device: "supersonic",
-        product: "SupersonicStock",
-        incremental: 0030706510011,
-        screenshots:
+        "name": "Stock 3.70.651.1 Deodexed",
+        "summary": "Rooted, zipaligned, with busybox, from teshxx",
+        "device": "supersonic",
+        "product": "SupersonicStock",
+        "incremental": "0030706510011",
+        "screenshots": 
         [
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-1.png",
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-2.png",
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png"
         ],
-        url: "http://goo-inside.me/supersonic/stock/3.70.651.1-rooted-deodexed-signed-teshxx.zip"
+        "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-rooted-deodexed-signed-teshxx.zip"
     },
     {
-        name: "Stock 3.70.651.1 Odexed",
-        summary: "Rooted, zipaligned, with busybox, from teshxx",
-        device: "supersonic",
-        product: "SupersonicStock",
-        incremental: 0030706510010,
-        screenshots:
+        "name": "Stock 3.70.651.1 Odexed",
+        "summary": "Rooted, zipaligned, with busybox, from teshxx",
+        "device": "supersonic",
+        "product": "SupersonicStock",
+        "incremental": "0030706510010",
+        "screenshots": 
         [
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-1.png",
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-2.png",
         "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png"
         ],
-        url: "http://goo-inside.me/supersonic/stock/3.70.651.1-rooted-odexed-signed-teshxx.zip"
+        "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-rooted-odexed-signed-teshxx.zip"
     },
     {
-        name: "Stock 3.30.651.3 Deodexed",
-        summary: "Rooted, zipaligned, with busybox, from xxbabiboi228xx",
-        device: "supersonic",
-        product: "SupersonicStock",
-        incremental: 0030306510031,
-        url: "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-deodexed-signed-xxbabiboi228xx.zip"
+        "name": "Stock 3.30.651.3 Deodexed",
+        "summary": "Rooted, zipaligned, with busybox, from xxbabiboi228xx",
+        "device": "supersonic",
+        "product": "SupersonicStock",
+        "incremental": "0030306510031",
+        "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-deodexed-signed-xxbabiboi228xx.zip"
     },
     {
-        name: "Stock 3.30.651.3 Odexed",
-        summary: "Rooted, zipaligned, with busybox, from xxbabiboi228xx",
-        device: "supersonic",
-        product: "SupersonicStock",
-        incremental: 0030306510030,
-        url: "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
+        "name": "Stock 3.30.651.3 Odexed",
+        "summary": "Rooted, zipaligned, with busybox, from xxbabiboi228xx",
+        "device": "supersonic",
+        "product": "SupersonicStock",
+        "incremental": "0030306510030",
+        "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
     },
     {
-        name: "Stock 3.26.651.6 Odexed",
-        summary: "Rooted, from netarchy",
-        device: "supersonic",
-        product: "SupersonicStock",
-        incremental: 0030266510060,
-        url: "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
-    },
-    // Radios
-    {
-        name: "Radio 2.15.00.11.19",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0215001119,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19.zip"
+        "name": "Stock 3.26.651.6 Odexed",
+        "summary": "Rooted, from netarchy",
+        "device": "supersonic",
+        "product": "SupersonicStock",
+        "incremental": "0030266510060",
+        "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
     },
     {
-        name: "Radio 2.15.00.09.01",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0215001119,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.09.01.zip"
+        "name": "Radio 2.15.00.11.19",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0215001119",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19.zip"
     },
     {
-        name: "Radio 2.15.00.07.28",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0215000728,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.28.zip"
+        "name": "Radio 2.15.00.09.01",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0215001119",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.09.01.zip"
     },
     {
-        name: "Radio 2.15.00.07.21",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0215000721,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.21.zip"
+        "name": "Radio 2.15.00.07.28",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0215000728",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.28.zip"
     },
     {
-        name: "Radio 2.05.00.06.10",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0205000610,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_2.05.00.06.10.zip"
+        "name": "Radio 2.15.00.07.21",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0215000721",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.21.zip"
     },
     {
-        name: "Radio 1.39.00.05.31",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0139000531,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_1.39.00.05.31.zip"
+        "name": "Radio 2.05.00.06.10",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0205000610",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.05.00.06.10.zip"
     },
     {
-        name: "Radio 1.36.00.04.02",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicRadio",
-        incremental: 0136000402,
-        url: "http://goo-inside.me/supersonic/radios/EVO_Radio_1.36.00.04.02.zip"
-    },
-    // WiMAX
-    {
-        name: "WiMAX 27167 R01",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicWiMAX",
-        incremental: 2716701,
-        url: "http://goo-inside.me/supersonic/radios/EVO_WiMAX_27167_R01.zip"
+        "name": "Radio 1.39.00.05.31",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0139000531",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.39.00.05.31.zip"
     },
     {
-        name: "WiMAX 26023 R01",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicWiMAX",
-        incremental: 2602301,
-        url: "http://goo-inside.me/supersonic/radios/EVO_WiMAX_26023_R01.zip"
+        "name": "Radio 1.36.00.04.02",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicRadio",
+        "incremental": "0136000402",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.36.00.04.02.zip"
     },
     {
-        name: "WiMAX 25641 R01",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicWiMAX",
-        incremental: 2564101,
-        url: "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25641_R01.zip"
+        "name": "WiMAX 27167 R01",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicWiMAX",
+        "incremental": "2716701",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_27167_R01.zip"
     },
     {
-        name: "WiMAX 25043 R01",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicWiMAX",
-        incremental: 2504301,
-        url: "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25043_R04.zip"
+        "name": "WiMAX 26023 R01",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicWiMAX",
+        "incremental": "2602301",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_26023_R01.zip"
     },
     {
-        name: "WiMAX 24722",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicWiMAX",
-        incremental: 2472200,
-        url: "http://goo-inside.me/supersonic/radios/EVO_WiMAX_24722.zip"
-    },
-    // PRI
-    {
-        name: "PRI 1.90_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicPRI",
-        incremental: 0190003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_PRI_1.90_003.zip"
+        "name": "WiMAX 25641 R01",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicWiMAX",
+        "incremental": "2564101",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25641_R01.zip"
     },
     {
-        name: "PRI 1.77_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicPRI",
-        incremental: 0177003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_PRI_1.77_003.zip"
+        "name": "WiMAX 25043 R01",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicWiMAX",
+        "incremental": "2504301",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25043_R04.zip"
     },
     {
-        name: "PRI 1.34_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicPRI",
-        incremental: 0134003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_PRI_1.34_003.zip"
-    },
-    // NV
-    {
-        name: "NV 1.90_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicNV",
-        incremental: 0190003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_NV_1.90_003.zip"
+        "name": "WiMAX 24722",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicWiMAX",
+        "incremental": "2472200",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_24722.zip"
     },
     {
-        name: "NV 1.77_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicNV",
-        incremental: 0177003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_NV_1.77_003.zip"
+        "name": "PRI 1.90_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicPRI",
+        "incremental": "0190003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.90_003.zip"
     },
     {
-        name: "NV 1.71_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicNV",
-        incremental: 0171003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_NV_1.71_003.zip"
+        "name": "PRI 1.77_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicPRI",
+        "incremental": "0177003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.77_003.zip"
     },
     {
-        name: "NV 1.40_003",
-        summary: "Needs S-OFF! Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicNV",
-        incremental: 0140003,
-        url: "http://goo-inside.me/supersonic/radios/EVO_NV_1.40_003.zip"
-    },
-    // HBOOT
-    {
-        name: "ENG Hboot 0.76.2000",
-        summary: "Recommended, supports Fastboot, can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicHboot",
-        incremental: 99999999,
-        url: "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.76.2000.zip"
+        "name": "PRI 1.34_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicPRI",
+        "incremental": "0134003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.34_003.zip"
     },
     {
-        name: "Hboot 2.10.0001",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicHboot",
-        incremental: 02100001,
-        url: "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.10.0001.zip"
+        "name": "NV 1.90_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicNV",
+        "incremental": "0190003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.90_003.zip"
     },
     {
-        name: "Hboot 2.02.0000",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicHboot",
-        incremental: 02020000,
-        url: "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.02.0000.zip"
+        "name": "NV 1.77_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicNV",
+        "incremental": "0177003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.77_003.zip"
     },
     {
-        name: "Hboot 0.97.0000",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicHboot",
-        incremental: 00970000,
-        url: "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.97.0000.zip"
+        "name": "NV 1.71_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicNV",
+        "incremental": "0171003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.71_003.zip"
     },
     {
-        name: "Hboot 0.93.0000",
-        summary: "Can brick if used wrong! From Calkulin",
-        device: "supersonic",
-        product: "SupersonicHboot",
-        incremental: 00930000,
-        url: "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.93.0000.zip"
+        "name": "NV 1.40_003",
+        "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicNV",
+        "incremental": "0140003",
+        "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.40_003.zip"
+    },
+    {
+        "name": "ENG Hboot 0.76.2000",
+        "summary": "Recommended, supports Fastboot, can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicHboot",
+        "incremental": "99999999",
+        "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.76.2000.zip"
+    },
+    {
+        "name": "Hboot 2.10.0001",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicHboot",
+        "incremental": "02100001",
+        "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.10.0001.zip"
+    },
+    {
+        "name": "Hboot 2.02.0000",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicHboot",
+        "incremental": "02020000",
+        "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.02.0000.zip"
+    },
+    {
+        "name": "Hboot 0.97.0000",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicHboot",
+        "incremental": "00970000",
+        "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.97.0000.zip"
+    },
+    {
+        "name": "Hboot 0.93.0000",
+        "summary": "Can brick if used wrong! From Calkulin",
+        "device": "supersonic",
+        "product": "SupersonicHboot",
+        "incremental": "00930000",
+        "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.93.0000.zip"
     }
     ]
 }
