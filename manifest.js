ed9b0bf3a57c65daf2c28e738d68d0f1b86bb599
DrMacinyasha <drmacinyasha@gmail.com>
2011-05-21 19:16:59 -0700

    Testing.
    Trying a new download site for goo-inside.me

--- manifest.js
+++ manifest.js
@@ -283,5 +283,19 @@
             "incremental": "00930000",
             "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.93.0000.zip" 
         } 
+        {
+            "name": "TESTING",
+            "summary": "GTFO",
+            "device": "supersonic",
+            "product": "SupersonicStock",
+            "modversion": "Supersonic-3.70.651.1-Deodexed",
+            "incremental": "0030706510011",
+            "screenshots": [
+                "http://dev.goo-inside.me/supersonic/stock/screenshots/003070651001-1.png",
+                "http://dev.goo-inside.me/supersonic/stock/screenshots/003070651001-2.png",
+                "http://dev.goo-inside.me/supersonic/stock/screenshots/003070651001-3.png" 
+            ],
+            "url": "http://dev.goo-inside.me/supersonic/stock/3.70.651.1-deodexed-signed.zip" 
+        } 
     ]
 }
