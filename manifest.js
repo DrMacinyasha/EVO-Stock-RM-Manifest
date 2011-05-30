04674215390e1402f4fa2e5eee8051147b9f5dbb
DrMacinyasha <drmacinyasha@gmail.com>
2011-05-30 15:53:13 -0700

    Remove incrementals
    Incrementals were depreciated by koush for ROM Manager. Only modversion is used now.
    https://github.com/koush/ROMManagerManifest/commit/dcce3b0a8668625669ff148bb2c9b8abc8deaef4

--- manifest.js
+++ manifest.js
@@ -9,13 +9,12 @@
             "device": "supersonic",
             "product": "SupersonicStock",
             "modversion": "Supersonic-3.70.651.1-Deodexed",
-            "incremental": "0030706510011",
             "screenshots": [
                 "http://goo-inside.me/supersonic/stock/screenshots/003070651001-1.png",
                 "http://goo-inside.me/supersonic/stock/screenshots/003070651001-2.png",
-                "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png" 
+                "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png"
             ],
-            "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-deodex-signed.zip" 
+            "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-deodex-signed.zip"
         },
         {
             "name": "Stock 3.70.651.1 Odexed",
@@ -23,265 +22,236 @@
             "device": "supersonic",
             "product": "SupersonicStock",
             "modversion": "Supersonic-3.70.651.1-Odexed",
-            "incremental": "0030706510010",
             "screenshots": [
                 "http://goo-inside.me/supersonic/stock/screenshots/003070651001-1.png",
                 "http://goo-inside.me/supersonic/stock/screenshots/003070651001-2.png",
-                "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png" 
+                "http://goo-inside.me/supersonic/stock/screenshots/003070651001-3.png"
             ],
-            "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-odex-signed.zip" 
+            "url": "http://goo-inside.me/supersonic/stock/3.70.651.1-odex-signed.zip"
         },
         {
             "name": "Stock 3.30.651.3 Deodexed",
-            "summary": "Rooted, zipaligned, busybox, from xxbabiboi228xx\nIncompatible with ClockworkMod 3.0",
+            "summary": "Rooted, zipaligned, busybox, from xxbabiboi228xx\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicStock",
             "modversion": "Supersonic-3.30.651.3-Deodexed",
-            "incremental": "0030306510031",
-            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-deodexed-signed-xxbabiboi228xx.zip" 
+            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-deodexed-signed-xxbabiboi228xx.zip"
         },
         {
             "name": "Stock 3.30.651.3 Odexed",
-            "summary": "Rooted, zipaligned, busybox, from xxbabiboi228xx\nIncompatible with ClockworkMod 3.0",
+            "summary": "Rooted, zipaligned, busybox, from xxbabiboi228xx\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicStock",
             "modversion": "Supersonic-3.30.651.3-Odexed",
-            "incremental": "0030306510030",
-            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip" 
+            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
         },
         {
             "name": "Stock 3.26.651.6 Odexed",
-            "summary": "Rooted, from netarchy\nIncompatible with ClockworkMod 3.0",
+            "summary": "Rooted, from netarchy\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicStock",
             "modversion": "Supersonic-3.26.651.6-Odexed",
-            "incremental": "0030266510060",
-            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip" 
+            "url": "http://goo-inside.me/supersonic/stock/3.30.651.3-rooted-odexed-signed-xxbabiboi228xx.zip"
         },
         {
             "name": "Combo Radio 2.15.00.11.19",
-            "summary": "Also updates WiMAX 27167 R01, PRI and NV 1.90_003. From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Also updates WiMAX 27167 R01, PRI and NV 1.90_003. From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicCombo",
             "modversion": "Supersonic-2.15.00.11.19-Combo",
-            "incremental": "0215001119",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19_WiMAX_27167_R01_PRI_NV_1.90_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19_WiMAX_27167_R01_PRI_NV_1.90_003.zip"
         },
         {
             "name": "Radio 2.15.00.11.19",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-2.15.00.11.19-Radio",
-            "incremental": "0215001119",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.11.19.zip"
         },
         {
             "name": "Radio 2.15.00.09.01",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-2.15.99.09.01-Radio",
-            "incremental": "0215001119",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.09.01.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.09.01.zip"
         },
         {
             "name": "Radio 2.15.00.07.28",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-2.15.00.07.28-Radio",
-            "incremental": "0215000728",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.28.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.28.zip"
         },
         {
             "name": "Radio 2.15.00.07.21",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-2.15.00.07.21-Radio",
-            "incremental": "0215000721",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.21.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.15.00.07.21.zip"
         },
         {
             "name": "Radio 2.05.00.06.10",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-2.05.00.06.10-Radio",
-            "incremental": "0205000610",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.05.00.06.10.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_2.05.00.06.10.zip"
         },
         {
             "name": "Radio 1.39.00.05.31",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-1.39.00.05.31-Radio",
-            "incremental": "0139000531",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.39.00.05.31.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.39.00.05.31.zip"
         },
         {
             "name": "Radio 1.36.00.04.02",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicRadio",
             "modversion": "Supersonic-1.36.00.04.02-Radio",
-            "incremental": "0136000402",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.36.00.04.02.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_Radio_1.36.00.04.02.zip"
         },
         {
             "name": "WiMAX 27167 R01",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicWiMAX",
             "modversion": "Supersonic-27167.01-WiMAX",
-            "incremental": "2716701",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_27167_R01.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_27167_R01.zip"
         },
         {
             "name": "WiMAX 26023 R01",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicWiMAX",
             "modversion": "Supersonic-26023.01-WiMAX",
-            "incremental": "2602301",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_26023_R01.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_26023_R01.zip"
         },
         {
             "name": "WiMAX 25641 R01",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicWiMAX",
             "modversion": "Supersonic-25641.01-WiMAX",
-            "incremental": "2564101",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25641_R01.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25641_R01.zip"
         },
         {
             "name": "WiMAX 25043 R01",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicWiMAX",
             "modversion": "Supersonic-25043.01-WiMAX",
-            "incremental": "2504301",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25043_R04.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_25043_R04.zip"
         },
         {
             "name": "WiMAX 24722",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicWiMAX",
             "modversion": "Supersonic-24722.00-WiMAX",
-            "incremental": "2472200",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_24722.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_WiMAX_24722.zip"
         },
         {
             "name": "PRI 1.90_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicPRI",
             "modversion": "Supersonic-1.90.003-PRI",
-            "incremental": "0190003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.90_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.90_003.zip"
         },
         {
             "name": "PRI 1.77_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicPRI",
             "modversion": "Supersonic-1.77.003-PRI",
-            "incremental": "0177003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.77_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.77_003.zip"
         },
         {
             "name": "PRI 1.34_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicPRI",
             "modversion": "Supersonic-1.34.003-PRI",
-            "incremental": "0134003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.34_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_PRI_1.34_003.zip"
         },
         {
             "name": "NV 1.90_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicNV",
             "modversion": "Supersonic-1.90.003-NV",
-            "incremental": "0190003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.90_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.90_003.zip"
         },
         {
             "name": "NV 1.77_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicNV",
             "modversion": "Supersonic-1.77.003-NV",
-            "incremental": "0177003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.77_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.77_003.zip"
         },
         {
             "name": "NV 1.71_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicNV",
             "modversion": "Supersonic-1.71.003-NV",
-            "incremental": "0171003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.71_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.71_003.zip"
         },
         {
             "name": "NV 1.40_003",
-            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Needs S-OFF! Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicNV",
             "modversion": "Supersonic-1.40.003-NV",
-            "incremental": "0140003",
-            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.40_003.zip" 
+            "url": "http://goo-inside.me/supersonic/radios/EVO_NV_1.40_003.zip"
         },
         {
             "name": "ENG Hboot 0.76.2000",
-            "summary": "Supports Fastboot, can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Supports Fastboot, can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicHboot",
             "modversion": "Supersonic-0.76.2000-HBOOT",
-            "incremental": "99999999",
-            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.76.2000.zip" 
+            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.76.2000.zip"
         },
         {
             "name": "Hboot 2.10.0001",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicHboot",
             "modversion": "Supersonic-2.10.0001-HBOOT",
-            "incremental": "02100001",
-            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.10.0001.zip" 
+            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.10.0001.zip"
         },
         {
             "name": "Hboot 2.02.0000",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicHboot",
             "modversion": "Supersonic-2.02.0000-HBOOT",
-            "incremental": "02020000",
-            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.02.0000.zip" 
+            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_2.02.0000.zip"
         },
         {
             "name": "Hboot 0.97.0000",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicHboot",
             "modversion": "Supersonic-0.97.0000-HBOOT",
-            "incremental": "00970000",
-            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.97.0000.zip" 
+            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.97.0000.zip"
         },
         {
             "name": "Hboot 0.93.0000",
-            "summary": "Can brick if used wrong! From Calkulin\nIncompatible with ClockworkMod 3.0",
+            "summary": "Can brick if used wrong! From Calkulin\u000aDowngrade to ClockworkMod 2.5.0.7 to flash",
             "device": "supersonic",
             "product": "SupersonicHboot",
             "modversion": "Supersonic-0.93.0000-HBOOT",
-            "incremental": "00930000",
-            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.93.0000.zip" 
-        } 
+            "url": "http://goo-inside.me/supersonic/hboot/EVO_HBoot_0.93.0000.zip"
+        }
     ]
 }
